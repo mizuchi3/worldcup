28cdac4757cb9cbb1b3bf49a65e6b7fc2c9c38f0
U-UNIBASEL\waterhou <waterhou@KL-KBTS01-PDW06.pz.unibas.ch>
2014-05-27 09:40:29 +0200

    add these

---
+++ predictor/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from predictor.models import *
+
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ('name','team')
+
+
+class ScorerInline(admin.TabularInline):
+    model = GoalCount
+
+
+class GameAdmin(admin.ModelAdmin):
+	list_display=('team_a','team_b','odds_a','odds_b','match_date','goals_a','goals_b')
+	inlines = [
+        ScorerInline,
+    ]
+
+class PredictionAdmin(admin.ModelAdmin):
+	list_display=('user','game','predict_a','predict_b','points_awarded')
+
+
+class BestPlayerAdmin(admin.ModelAdmin):
+	list_display = ('user', 'player')
+
+
+
+admin.site.register(Player,PlayerAdmin)
+admin.site.register(Game,GameAdmin)
+admin.site.register(Prediction,PredictionAdmin)
+admin.site.register(BestPlayer,BestPlayerAdmin)
+
---
+++ predictor/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Player(models.Model):
+	name = models.CharField(max_length=65)
+	team = models.CharField(max_length=65,default=None,blank=True)
+	def __unicode__(self):
+		return '%s (%s)' %(self.name,self.team)
+	class Meta:
+		db_table = 'players'
+
+
+class Game(models.Model):
+    team_a = models.CharField(max_length=65)
+    team_b = models.CharField(max_length=65)
+    odds_a = models.FloatField(null=True,blank=True)
+    odds_b = models.FloatField(null=True,blank=True)
+    match_date = models.DateTimeField('date published')
+    goals_a = models.IntegerField(null=True,blank=True)
+    goals_b = models.IntegerField(null=True,blank=True)
+    scorer = models.ManyToManyField(Player,through='GoalCount',default=None,blank=True)
+    def __unicode__(self):
+		return '%s v %s' %(self.team_a,self.team_b)
+    class Meta:
+        db_table = 'games'
+
+class GoalCount(models.Model):
+	player = models.ForeignKey(Player)
+	game = models.ForeignKey(Game)
+	goals_scored = models.IntegerField(default=0)
+
+class Prediction(models.Model):
+	user = models.ForeignKey(User)
+	game = models.ForeignKey(Game)
+	predict_a = models.IntegerField(default=0)
+	predict_b = models.IntegerField(default=0)
+	points_awarded = models.FloatField(null=True,blank=True)
+	class Meta:
+		db_table = 'predictions'
+
+class BestPlayer(models.Model):
+	user = models.ForeignKey(User)
+	player = models.ForeignKey(Player)
+	class Meta:
+		db_table = 'bestplayer'
+
+
